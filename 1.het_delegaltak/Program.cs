c8b1297deeb190c7ab2520d799f226add57aa0eb
Por Levente <porleventeferenc@gmail.com>
2025-09-09 23:04:44 +0200

    Add project files.

---
+++ 1.het_delegaltak/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1.het_delegaltak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
