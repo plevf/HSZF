44ba947f3acf22b647fde9b236fca5a87e90ca61
Por Levente <porleventeferenc@gmail.com>
2025-10-03 17:51:14 +0200

    a

--- minta_zh/CarCounterByBrandEventArgs.cs
+++ minta_zh/CarCounterByBrandEventArgs.cs
@@ -9,10 +9,12 @@ namespace minta_zh
     public class CarCounterByBrandEventArgs : AllCars
     {
         List<Car> cars;
+        public string FileName { get; private set; }
         public string Brand { get; private set; }
         public int Count { get; private set; }
         public CarCounterByBrandEventArgs(string fileName, string brand) : base(fileName)
         {
+            FileName = fileName;
             cars = ReadFile(fileName);
             Count = cars.Count(p => p.Brand == brand);
             Brand = brand;
--- minta_zh/Program.cs
+++ minta_zh/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace minta_zh
+﻿using System.Threading.Channels;
+
+namespace minta_zh
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            EventHandler<CarCounterByBrandEventArgs> handler = (sender, args) => Console.WriteLine($"")
         }
         
     }
