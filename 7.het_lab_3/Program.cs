3f987675c45573c244587aa05c32f3fbd80c254b
Por Levente <porleventeferenc@gmail.com>
2025-10-22 16:36:42 +0200

    Reflection

--- 7.het_lab_3/Program.cs
+++ 7.het_lab_3/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace _7.het_lab_3
+﻿
+
+namespace _7.het_lab_3
 {
+
+    // reflection
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public int Age { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Reflection_Basics();
+            BuiltInAttributes();
+        }
+
+        private static void BuiltInAttributes()
+        {
+
+        }
+
+        private static void Reflection_Basics()
+        {
+            var p = new Person
+            {
+                Id = 1,
+                LastName = "Doe",
+                FirstName = "John",
+                Age = 30
+            };
+
+            var t = typeof(Person);
+
+            Console.WriteLine("tpye: " + t.FullName);
+
+            foreach (var prop in t.GetProperties())
+            {
+                Console.WriteLine(" - " + prop.Name + " : " + prop.PropertyType.Name); // property neve plusz a tipusa
+            }
+
+            var lastNameProp = t.GetProperty("LastName");
+            if(lastNameProp != null)
+            {
+                Console.WriteLine("original: " + lastNameProp.GetValue(p));
+                lastNameProp.SetValue(p, "Smith");
+                Console.WriteLine("modified: " + lastNameProp.GetValue(p));
+            }
         }
     }
 }
