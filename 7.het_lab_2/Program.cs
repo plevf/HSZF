8319f6fc1950d7d5fe805faaae392d68a44c3aa3
Por Levente <porleventeferenc@gmail.com>
2025-10-22 15:32:14 +0200

    7.het init

---
+++ 7.het_lab_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _7.het_lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
