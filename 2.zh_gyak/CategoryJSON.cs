8e979f0bbf528392646120d542bffba1fc5c9f10
Por Levente <porleventeferenc@gmail.com>
2025-11-04 16:53:45 +0100

    json class fixed

--- 2.zh_gyak/CategoryJSON.cs
+++ 2.zh_gyak/CategoryJSON.cs
@@ -7,35 +7,38 @@ using System.Threading.Tasks;
 namespace _2.zh_gyak
 {
 
-    public class RootobjectJ
-    {
-        public ProductpackageJ ProductPackageJ { get; set; }
-    }
+    //public class RootobjectJ
+    //{
+    //    public ProductpackageJ ProductPackageJ { get; set; }
+    //}
 
-    public class ProductpackageJ
-    {
-        public CategoriesJ CategoriesJ { get; set; }
-    }
+    //public class ProductpackageJ
+    //{
+    //    public CategoriesJ CategoriesJ { get; set; }
+    //}
 
-    public class CategoriesJ
-    {
-        public Category[] CategoryJ { get; set; }
-    }
+    //public class CategoriesJ
+    //{
+    //    public Category[] CategoryJ { get; set; }
+    //}
 
     public class CategoryJ
     {
-        public string NameJ { get; set; }
-        public ProductsJ ProductsJ { get; set; }
+        public int Id { get; set; }
+        public string NameJ { get; set; } = string.Empty;
+        public List<ProductJ> ProductsJ { get; set; } = new List<ProductJ>();
     }
 
-    public class ProductsJ
-    {
-        public Product[] ProductJ { get; set; }
-    }
+    //public class ProductsJ
+    //{
+    //    public Product[] ProductJ { get; set; }
+    //}
 
     public class ProductJ
     {
-        public string SkuJ { get; set; }
+        public int Id { get; set; }
+        [RequiredNonEmpty]
+        public string? SkuJ { get; set; }
         public string NameJ { get; set; }
         public int PriceJ { get; set; }
     }
