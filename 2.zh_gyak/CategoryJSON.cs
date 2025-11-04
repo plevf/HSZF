2e216cb02af67aa39b036a4b54cacbde1c3bb18e
Por Levente <porleventeferenc@gmail.com>
2025-11-04 16:49:27 +0100

    jsonClass

---
+++ 2.zh_gyak/CategoryJSON.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.zh_gyak
+{
+
+    public class RootobjectJ
+    {
+        public ProductpackageJ ProductPackageJ { get; set; }
+    }
+
+    public class ProductpackageJ
+    {
+        public CategoriesJ CategoriesJ { get; set; }
+    }
+
+    public class CategoriesJ
+    {
+        public Category[] CategoryJ { get; set; }
+    }
+
+    public class CategoryJ
+    {
+        public string NameJ { get; set; }
+        public ProductsJ ProductsJ { get; set; }
+    }
+
+    public class ProductsJ
+    {
+        public Product[] ProductJ { get; set; }
+    }
+
+    public class ProductJ
+    {
+        public string SkuJ { get; set; }
+        public string NameJ { get; set; }
+        public int PriceJ { get; set; }
+    }
+
+}
